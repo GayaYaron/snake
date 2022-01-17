7ec5d07fcfc7cc25fb34fc5137501f734ca159be
Gaya <yarongaya@gmail.com>
2022-01-17 22:40:05 +0200

    fixed board sizing

--- src/pages/gamePage.js
+++ src/pages/gamePage.js
@@ -2,7 +2,7 @@ import { Game } from "../components/game";
 
 export function GamePage(props) {
     return (
-        <div className="no-scroll">
+        <div className="gamePage">
             <Game />
         </div>
     )
--- src/pages/generalPage.js
+++ src/pages/generalPage.js
@@ -4,7 +4,7 @@ import { GamePage } from './gamePage';
 export function GeneralPage(props) {
     return (
         <div className='d-flex justify-content-center mt-5 w-100'>
-            <div className="col-12 col-sm-8 col-md-6 col-lg-4">
+            <div className="col-12 col-md-6 col-lg-4">
                 <GamePage />
             </div>
         </div>
