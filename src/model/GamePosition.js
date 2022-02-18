4f3e5f9122288ceccf9892d05f5679d8fabcef2c
Gaya <yarongaya@gmail.com>
2022-02-18 23:58:39 +0200

    turned game to game page and finished the score system

--- src/model/GamePosition.js
+++ src/model/GamePosition.js
@@ -1,8 +1,9 @@
 export class GamePosition {
-    constructor(snake, food, delay, status) {
+    constructor(snake, food, delay, score, status) {
         this.snake = snake;
         this.food = food;
         this.delay = delay;
         this.status = status;
+        this.score = score;
     }
 }
