af52c5abdfa6d0889db676860b393b81d8dacb16
Gaya <yarongaya@gmail.com>
2022-02-20 17:36:18 +0200

    created base structure for main menu

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { MainMenu } from "../components/mainMenu";
 
 export function HomePage(props){
     return(
         <div>
+            <MainMenu />
             <Outlet />
         </div>
     )
