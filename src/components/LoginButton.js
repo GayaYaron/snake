143136bb96a51d09db3dd45cdddb6cf47eebd3fe
Gaya <yarongaya@gmail.com>
2022-03-18 19:13:51 +0200

    created login button

---
+++ src/components/LoginButton.js
@@ -0,0 +1,41 @@
+import { useNavigate } from 'react-router-dom';
+import { logout } from "../redux/actions/index";
+import { connect } from "react-redux";
+
+function LoginButtonComp(props) {
+    const navigate = useNavigate();
+
+    const goToLogin = () => {
+        navigate(props.pathBack+"login");
+    }
+
+    const logoutClicked = () => {
+        props.logout();
+    }
+
+    if(props.loginInfo.info) {
+        return(
+            <button type="button" className={props.btnClass} onClick={logoutClicked}>Logout</button>
+        );
+    }else {
+        return(
+            <button type="button" className={props.btnClass} onClick={goToLogin}>Login/Register</button>
+        );
+    };
+}
+
+const mapStateToProps = (state) => {
+    return {
+        loginInfo: state.loginInfo
+    };
+};
+
+const mapDispatchActions = () => {
+    return {
+        logout
+    };
+};
+
+export const LoginButton = connect(
+    mapStateToProps, mapDispatchActions()
+)(LoginButtonComp);
