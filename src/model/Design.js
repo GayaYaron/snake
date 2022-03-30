25afc2b4051128da2e55f2e4f3a1d19f77422596
Gaya <yarongaya@gmail.com>
2022-03-30 20:44:49 +0300

    created design in model

---
+++ src/model/Design.js
@@ -0,0 +1,9 @@
+export class Design {
+    constructor(id, name, snakeColor, borderColor, foodColor) {
+        this.id = id;
+        this.name = name;
+        this.snakeColor = snakeColor;
+        this.borderColor = borderColor;
+        this.foodColor = foodColor;
+    }
+}
