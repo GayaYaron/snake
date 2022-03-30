fc4e98b74304d988452b381c796dc507496a639d
Gaya <yarongaya@gmail.com>
2022-03-30 22:50:13 +0300

    finished creating design view

--- src/components/DesignView.js
+++ src/components/DesignView.js
@@ -1,13 +1,25 @@
 import "../styles/game.css"
+import { indexIsBorder } from "../service/borderService";
+import { CellBoard } from "./CellsBoard";
 
 export function DesignView(props) {
+    const boardSize = 64;
+
     const cellColor = (index) => {
-        
+        if (index === 29) {
+            return props.design.foodColor;
+        } else if (index >= 42 && index <= 44) {
+            return props.design.snakeColor;
+        } else if (indexIsBorder(index, boardSize, 8, 8)) {
+            return props.design.borderColor;
+        } else {
+            return "backDefault";
+        }
     }
 
-    return(
-        <div className="miniBoard" id={props.id}>
-            
+    return (
+        <div className="miniBoard" id={props.design.id}>
+            <CellBoard boardSize={boardSize} getCellColor={cellColor}/>
         </div>
     )
 }
