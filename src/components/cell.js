6f4f9a0adb0b0da7fea9bb362dfd72511361e1da
Gaya <yarongaya@gmail.com>
2022-03-30 21:48:50 +0300

    minimised cell comp

--- src/components/cell.js
+++ src/components/cell.js
@@ -1,27 +1,6 @@
 import "../styles/game.css";
-import { connect } from "react-redux";
 
 export function Cell(props) {
-    const getCellClass = () => {
-        // let classString = "cell ";
-        // if (props.cellColor) {
-        //     return classString + cellColor;
-        // } else {
-        //     switch (props.role) {
-        //         case "BORDER":
-        //             return classString + props.colors.border;
-        //         case "FOOD":
-        //             return classString + props.colors.food;
-        //         case "SNAKE":
-        //             return classString + props.colors.snake;
-        //         default:
-        //             return classString + "backDefault";
-        //     }
-        // }
-
-        return "cell " + props.color;
-    }
-
     return (
         <div className={"cell " + props.color} />
     )
